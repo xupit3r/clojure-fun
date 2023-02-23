e401d63061518502d198c48e47b11d7ea5ee57a6
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-23 06:13:53 -0500

    the start of some clojure fun!

---
+++ src/clojure_fun/core.clj
@@ -0,0 +1,9 @@
+(ns clojure-fun.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(defn joe [x] (+ x 1))
---
+++ test/clojure_fun/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-fun.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-fun.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
