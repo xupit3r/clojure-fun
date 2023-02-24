8d7cdf5d9b76e7f1d2ae7fc0a2153d1cd273e812
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-24 05:14:27 -0500

    played around with simple data retrieval/manipulation

--- src/clojure_fun/core.clj
+++ src/clojure_fun/core.clj
@@ -3,8 +3,32 @@
             [compojure.core :as comp]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults, site-defaults]]
-            [clojure.pprint :as pp])
+            [clojure.pprint :as pp]
+            [clojure.data.json :as json])
   (:gen-class))
+
+; pretend db shhh
+(def people-collection (atom []))
+
+(defn db-add-person [first last]
+  (swap! people-collection conj {:first first :last last}))
+
+; prepopulate that bad boi of a collection
+(db-add-person "Joe" "D'Alessandro")
+(db-add-person "Joe" "Schmoe")
+
+(defn get-people [req]
+  {:status 200
+   :headers {"Content-Type" "text/json"}
+   :body (str (json/write-str @people-collection))})
+
+(defn add-person [req]
+  {:status 200
+   :headers {"Content-Type" "text/json"}
+   :body (let [params (:params req)]
+           (str 
+            (json/write-str 
+             (db-add-person (:first params) (:last params)))))})
 
 ; Simple Body Page
 (defn simple-body-page [req]
@@ -20,9 +44,19 @@
              (pp/pprint req)
              (str "Request Object: " req))})
 
+(defn hello-name [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (->
+          (pp/pprint req)
+          (str "Hello " (:name (:params req))))})
+
 (comp/defroutes app-routes
   (comp/GET "/" [] simple-body-page)
   (comp/GET "/request" [] request-example)
+  (comp/GET "/hello" [] hello-name)
+  (comp/GET "/people" [] get-people)
+  (comp/GET "/people/add" [] add-person)
   (route/not-found "Error, page not found!"))
 
 (defn -main
