9394a8f9c1d8ada35b63d0e513d54f9d9115518d
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-24 04:24:07 -0500

    added some stuff to paly with web/api things

--- project.clj
+++ project.clj
@@ -3,5 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns clojure-fun.core})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 ; Compojure - A basic routing library
+                 [compojure "1.7.0"]
+                 ; Our Http library for client/server
+                 [http-kit "2.6.0"]
+                 ; Ring defaults - for query params etc
+                 [ring/ring-defaults "0.3.4"]
+                 ; Clojure data.JSON library
+                 [org.clojure/data.json "0.2.6"]]
+  :repl-options {:init-ns clojure-fun.core}
+  :main clojure-fun.core)
--- src/clojure_fun/core.clj
+++ src/clojure_fun/core.clj
@@ -1,9 +1,36 @@
-(ns clojure-fun.core)
+(ns clojure-fun.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :as comp]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults, site-defaults]]
+            [clojure.pprint :as pp])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; Simple Body Page
+(defn simple-body-page [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Hello World"})
 
+; request-example
+(defn request-example [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (->>
+             (pp/pprint req)
+             (str "Request Object: " req))})
 
-(defn joe [x] (+ x 1))
+(comp/defroutes app-routes
+  (comp/GET "/" [] simple-body-page)
+  (comp/GET "/request" [] request-example)
+  (route/not-found "Error, page not found!"))
+
+(defn -main
+  "This is our main entry point"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http://127.0.0.1:" port "/"))))
